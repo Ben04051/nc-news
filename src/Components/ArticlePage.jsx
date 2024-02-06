60742def059d3bcf06c79036d152a5b8db672257
Ben04051 <benmitchell0405@hotmail.co.uk>
2024-02-06 10:29:57 +0000

    complete task 6 view comments associated with an article

--- src/Components/ArticlePage.jsx
+++ src/Components/ArticlePage.jsx
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom"
-import { getArticle, getAuthorName } from "../../utils/utils"
+import { getArticle, getArticleComments, getAuthorName, getUserPhotos } from "../../utils/utils"
 import { useEffect, useState } from "react"
 import './articlePage.css'
 
@@ -7,6 +7,8 @@ export default function ArticlePage () {
     const {article_id} = useParams()
     const [articleInfo, setArticleInfo] = useState([])
     const [authorName, setAuthorName] = useState("")
+    const [articleComments, setArticleComments] = useState([])
+    const [userPhotos, setUserPhotos] = useState([])
      
     useEffect(() => {
         getArticle(article_id).then((response) => {
@@ -14,6 +16,15 @@ export default function ArticlePage () {
             return getAuthorName(response.author)
         }).then((author) => {
            setAuthorName(author)
+           return getArticleComments(article_id)
+        }).then((comments) => {
+            setArticleComments(comments)
+            const userNames = comments.map((comment) => {
+                return comment.author
+            })
+            return getUserPhotos(userNames)
+        }).then((photos) => {
+            setUserPhotos(photos)
         })
     }, [])
 
@@ -28,7 +39,19 @@ export default function ArticlePage () {
         </footer>
         <p>{articleInfo.body}</p>
         <p className = "votes">Votes: {articleInfo.votes} <a><button>Vote</button></a></p>
-
-
+        <h4>Comments:</h4>
+        <ul className='comments'>
+            {articleComments.map((comment, index) => {
+        return(
+        <li key= {comment.comment_id}>
+        <div className="comment-header">
+        <img className= "profile-picture"src ={userPhotos[index]} alt={`${comment.author}'s profile picture'`}></img>
+        <p className="author">{comment.author}</p>
+        <p className="created-at">{comment.created_at ? articleInfo.created_at.slice(0,10) : ``}</p>
+        </div>
+        <p className="body">{comment.body}</p>
+        </li>)
+             })}
+        </ul>
     </div>
 }
